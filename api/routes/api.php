d7809385f8613357558f908b687253ec6f5478f3
Kai Nguyen <kien.nt@live.com>
2020-12-22 16:50:13 +1030

    Added laravel/passport

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
+
+Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
+
+Route::apiResource('/ceo', 'App\Http\Controllers\API\CEOController')->middleware('auth:api');
